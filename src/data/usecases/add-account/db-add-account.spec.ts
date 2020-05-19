22ee5c07a88e6dc1560d457ac24dd36abccdd98a
Andre Glatz <andreglatz@outlook.com>
2020-05-19 15:10:37 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -94,21 +94,21 @@ describe('DbAddAccount Usecase', () => {
     })
   })
 
-  test('Should throw if Encrypter throws', async () => {
-    const { sut, addAccountRepositoryStub } = makeSut()
-
-    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
-      new Promise(
-        (resolve, reject) => reject(new Error())
-      )
-    )
+  test('Should return an account if on success', async () => {
+    const { sut } = makeSut()
 
     const accountData = {
       name: 'valid_name',
       email: 'valid_email@mail.com',
       password: 'valid_password'
     }
-    const promise = sut.add(accountData)
-    expect(promise).rejects.toThrow()
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
   })
 })
