47c4646eddcb12fd43f7d341f68e2757030272d7
Andre Glatz <andreglatz@outlook.com>
2020-05-22 18:00:30 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/criptofraphy/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+import { Encrypter } from '../../../data/protocols/criptofraphy/encrypter'
+
+export class JwtAdapter implements Encrypter {
+  private readonly secret: string
+
+  constructor (secret: string) {
+    this.secret = secret
+  }
+
+  encrypt (value: string): string {
+    jwt.sign({ id: value }, this.secret)
+    return ''
+  }
+}
