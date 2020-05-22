6d1922ddbd971abca90936b527f6bd6eec58e852
Andre Glatz <andreglatz@outlook.com>
2020-05-22 17:29:27 -0300

    refactor: move bcrypt-adapter files to a folder

--- src/infra/criptofraphy/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptofraphy/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
-import { Hasher } from '../../data/protocols/criptofraphy/hasher'
+import { Hasher } from '../../../data/protocols/criptofraphy/hasher'
 import bcrypt from 'bcrypt'
-import { HashComparer } from '../../data/protocols/criptofraphy/hash-comparer'
+import { HashComparer } from '../../../data/protocols/criptofraphy/hash-comparer'
 
 export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number
