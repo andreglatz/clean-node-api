76b1ec2dce78bda5404fca35a877f0888a429737
Andre Glatz <andreglatz@outlook.com>
2020-05-28 13:20:16 -0300

    test: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -106,7 +106,7 @@ describe('Account Mongo Repository', () => {
       expect(account?.password).toBe('any_password')
     })
 
-    test('Should return an account on loadByToken withrole', async () => {
+    test('Should return an account on loadByToken with role', async () => {
       const sut = makeSut()
       await accountCollection.insertOne({
         name: 'any_name',
@@ -123,6 +123,12 @@ describe('Account Mongo Repository', () => {
       expect(account?.name).toBe('any_name')
       expect(account?.email).toBe('any_email@mail.com')
       expect(account?.password).toBe('any_password')
+    })
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
     })
   })
 })
