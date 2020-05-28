ca4c6ee997a80388a2a941b93da05cd90be6dbcb
Andre Glatz <andreglatz@outlook.com>
2020-05-28 18:07:04 -0300

    feat: ensure LoadSurveysController calls LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-useless-constructor */
+import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-controller-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (
+    private readonly loadSurveys: LoadSurveys
+  ) {}
+
+  async handle (httpRequest: HttpRequest) : Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return null
+  }
+}
