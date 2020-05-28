bee0c3f3a4b9a760eecd2c34ae7b96ef6278038b
Andre Glatz <andreglatz@outlook.com>
2020-05-28 14:58:17 -0300

    test: ensure add survey route returns 204 if valid  access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -15,8 +18,11 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    surveyCollection = await MongoHelper.getCollection('accounts')
+    surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -38,6 +44,39 @@ describe('Login Routes', () => {
         .post('/api/surveys')
         .send(surveyRequest)
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const accountFake = {
+        name: 'André',
+        email: 'andre.glatz@gmail.com',
+        password: '123',
+        role: 'admin'
+      }
+
+      const account = await accountCollection.insertOne(accountFake)
+      const id = account.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+
+      const surveyRequest = {
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }
+        ]
+      }
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send(surveyRequest)
+        .expect(204)
     })
   })
 })
