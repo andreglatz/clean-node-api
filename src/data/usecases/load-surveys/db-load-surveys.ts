68190af67611574610b4f45aa6cb29a19fe6b3a9
Andre Glatz <andreglatz@outlook.com>
2020-05-29 10:47:58 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-useless-constructor */
+import { LoadSurveys } from '../../../domain/usercases/load-surveys'
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (
+    private readonly loadSurveysRepository: LoadSurveysRepository
+  ) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRepository.loadAll()
+    return null
+  }
+}
