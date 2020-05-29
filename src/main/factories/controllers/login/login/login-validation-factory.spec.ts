c753a6e42389c6b15cca1325095837fd25a303dd
Andre Glatz <andreglatz@outlook.com>
2020-05-29 17:56:27 -0300

    refactor: change mock path

--- src/main/factories/controllers/login/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login/login-validation-factory.spec.ts
@@ -4,7 +4,7 @@ import { EmailValidation, ValidationComposite, RequiredFieldValidation } from '@
 import { EmailValidator } from '@/validation/protocols/email-validator'
 import { Validation } from '@/presentation/protocols/validation'
 
-jest.mock('../../../../../validation/validators/validation-composite')
+jest.mock('@/validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
