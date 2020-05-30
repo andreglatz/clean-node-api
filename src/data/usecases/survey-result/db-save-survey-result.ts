d5dcff0ffd45f401ba8c0d888249384e88fea979
Andre Glatz <andreglatz@outlook.com>
2020-05-30 12:22:47 -0300

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ src/data/usecases/survey-result/db-save-survey-result.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-useless-constructor */
+import { SaveSurveyResult, SaveSurveyResultModel, SurveyResultModel, SaveSurveyResultRepository } from './db-save-survey-result-protocols'
+
+export class DbSaveSurveyResult implements SaveSurveyResult {
+  constructor (
+    private readonly saveSurveyResultRepository: SaveSurveyResultRepository
+  ) {}
+
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    await this.saveSurveyResultRepository.save(data)
+    return null
+  }
+}
