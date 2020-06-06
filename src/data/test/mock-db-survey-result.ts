c6bb12866086255b4d1d85c1fccd11618a9a4a21
Andre Glatz <andreglatz@outlook.com>
2020-06-06 12:05:31 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct value

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,10 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { SaveSurveyResultRepository, SurveyResultModel, SaveSurveyResultParams } from '../usecases/survey-result/db-save-survey-result-protocols'
+import { SaveSurveyResultRepository, SurveyResultModel, SaveSurveyResultParams } from '../usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 import { mockSurveyResultModel } from '@/domain/test'
+import { LoadSurveyResultRepository } from '../protocols/db/survey-result/load-survey-result-repository'
 
 export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
   class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
     async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel())
+    }
+  }
+  return new SaveSurveyResultRepositoryStub()
+}
+
+export const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
       return Promise.resolve(mockSurveyResultModel())
     }
   }
