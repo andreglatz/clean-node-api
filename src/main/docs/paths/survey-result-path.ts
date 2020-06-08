5e5235f13638e9bce0a955795c10522678451756
Andre Glatz <andreglatz@outlook.com>
2020-06-08 19:56:29 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -6,7 +6,7 @@ export const surveyResultPath = {
       }
     ],
     tags: ['Enquete'],
-    summary: 'API para cirar a resposta de uma enquete',
+    summary: 'API para criar a resposta de uma enquete',
     requestBody: {
       content: {
         'application/json': {
@@ -16,6 +16,44 @@ export const surveyResultPath = {
         }
       }
     },
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
     parameters: [{
       in: 'path',
       name: 'surveyId',
